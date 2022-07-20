5d150fb5cba58469db97c71c7b437fe9d161c256
Mohamed Hashim <mahmed@mobiquityinc.com>
2022-07-20 15:52:18 +0200

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Mohamed Ahmed on 20/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
