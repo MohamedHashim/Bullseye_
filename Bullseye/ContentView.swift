d90b1f33a4bc3f781c603530923f862e366b18b1
Mohamed Hashim <mahmed@mobiquityinc.com>
2022-07-20 16:33:13 +0200

    Init app

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -9,13 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("🎯🎯🎯\nPut the bullyseye as close as you can to")
+                .padding()
+            Text("89")
+            HStack {
+                Text("1")
+                Slider(value: .constant(50), in: 0.0...100.0)
+                Text("100")}
+            Button(action:{}) {
+                Text("Hit me")
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        ContentView().previewLayout(.fixed(width: 568, height: 320))
     }
 }
