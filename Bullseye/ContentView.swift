29687936ac9968b9e7dc687e455d24a65a699d88
Mohamed Hashim <mahmed@mobiquityinc.com>
2022-07-26 16:36:13 +0200

    Implement the alert dialog

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -8,18 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var alertIsVisible : Bool = false
+    
     var body: some View {
         VStack {
             Text("🎯🎯🎯\nPut the bullyseye as close as you can to")
-                .padding()
+                .bold()
+                .kerning(2.0)
+                .multilineTextAlignment(.center)
+                .font(.footnote)
             Text("89")
+                .kerning(-1.0)
+                .font(.largeTitle)
+                .fontWeight(.black)
             HStack {
                 Text("1")
+                    .font(.body)
+                    .fontWeight(.bold)
                 Slider(value: .constant(50), in: 0.0...100.0)
-                Text("100")}
-            Button(action:{}) {
+                Text("100")
+                .font(.body) //defualt font is body 18px
+                .fontWeight(.bold)}
+            Button(action:{
+                self.alertIsVisible = true
+                print("Hello, SwiftUI!")
+            }) {
                 Text("Hit me")
-            }
+            }.alert(isPresented: $alertIsVisible, content: {
+                return Alert(title: Text("Hello Title"), message: Text("Thank you!"), dismissButton: .default(Text("Bye")))
+            })
         }
     }
 }
